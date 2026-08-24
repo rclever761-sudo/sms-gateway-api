12f6aed10e840ec394faa11279d77b1b682478f3
rclever761-sudo <rclever761@gmail.com>
2026-08-24 13:08:53 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
-$host = "mysql-ddc4692-rclever761-3f21.aivencloud.com";
-$dbname = "defaultdb";
-$username = "avnadmin";
-$password = "AVNS_Q1eviPJ1owGEHyV5PCy"; // Paste your copied password here
-$port = "23985";
+$host = 'mysql-ddc4692-rclever761-3f21.aivencloud.com';
+$dbname = 'defaultdb';
+$username = 'avnadmin';
+$password = 'AVNS_Q1eviPJ1owGEHyV5PCy'; // Paste your copied Aiven password here
+$port = '23985';
 
 try {
     $pdo = new PDO(
